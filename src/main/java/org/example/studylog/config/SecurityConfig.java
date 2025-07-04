2c96d93cf329d93560c2ffc74524a83aeea377cc
정지윤 <samtori3849@gmail.com>
2025-07-04 13:35:43 +0900

    [Feat] 로그인 성공/실패 핸들러 추가

--- src/main/java/org/example/studylog/config/SecurityConfig.java
+++ src/main/java/org/example/studylog/config/SecurityConfig.java
@@ -1,16 +1,36 @@
 package org.example.studylog.config;
 
+import org.example.studylog.jwt.JWTFilter;
+import org.example.studylog.jwt.JWTUtil;
+import org.example.studylog.oauth2.CustomFailureHandler;
+import org.example.studylog.oauth2.CustomSuccessHandler;
+import org.example.studylog.service.oauth.CustomOAuth2UserService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.client.web.OAuth2LoginAuthenticationFilter;
 import org.springframework.security.web.SecurityFilterChain;
+
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+    private final CustomOAuth2UserService customOAuth2UserService;
+    private final CustomSuccessHandler customSuccessHandler;
+    private final CustomFailureHandler customFailureHandler;
+    private final JWTUtil jwtUtil;
+
+    public SecurityConfig(CustomOAuth2UserService customOAuth2UserService, CustomSuccessHandler customSuccessHandler, JWTUtil jwtUtil, CustomFailureHandler customFailureHandler) {
+        this.customOAuth2UserService = customOAuth2UserService;
+        this.customSuccessHandler = customSuccessHandler;
+        this.customFailureHandler = customFailureHandler;
+        this.jwtUtil = jwtUtil;
+    }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
@@ -27,14 +47,24 @@ public class SecurityConfig {
         http
                 .httpBasic((auth) -> auth.disable());
 
-        // oauth2
+        // JWTFilter 추가
         http
-                .oauth2Login(Customizer.withDefaults());
+                .addFilterAfter(new JWTFilter(jwtUtil), OAuth2LoginAuthenticationFilter.class);
+
+        // oauth2
+
+        http
+                .oauth2Login((oauth2) -> oauth2
+                        .userInfoEndpoint((UserInfoEndpointConfig) -> UserInfoEndpointConfig
+                                .userService(customOAuth2UserService))
+                        .successHandler(customSuccessHandler)
+                        .failureHandler(customFailureHandler)
+                );
 
         // 경로별 인가 작업
         http
                 .authorizeHttpRequests((auth) -> auth
-                        .requestMatchers("/").permitAll()
+                        .requestMatchers("/", "/login", "/main").permitAll()
                         .anyRequest().authenticated());
 
         // 세션 설정 : STATELESS
