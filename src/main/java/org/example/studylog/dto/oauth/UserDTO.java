634e70f55748f9d4142774e27a25d508fe997e91
정지윤 <samtori3849@gmail.com>
2025-07-04 13:23:42 +0900

    [Feat] CustomOAuth2UserService에서 유저 정보 매핑 DTO

---
+++ src/main/java/org/example/studylog/dto/oauth/UserDTO.java
@@ -0,0 +1,13 @@
+package org.example.studylog.dto.oauth;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UserDTO {
+
+    private String role;
+    private String name;
+    private String username;
+}
