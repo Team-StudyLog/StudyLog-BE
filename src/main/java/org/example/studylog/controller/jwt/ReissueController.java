6f45461d8429a9920c87373d8884b50153388d12
정지윤 <samtori3849@gmail.com>
2025-07-06 16:53:38 +0900

    [Refactor] refresh 토큰 검증/교체 로직을 TokenService로 분리

--- src/main/java/org/example/studylog/controller/jwt/ReissueController.java
+++ src/main/java/org/example/studylog/controller/jwt/ReissueController.java
@@ -4,20 +4,27 @@ import io.jsonwebtoken.ExpiredJwtException;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.example.studylog.entity.RefreshEntity;
 import org.example.studylog.jwt.JWTUtil;
+import org.example.studylog.repository.RefreshRepository;
+import org.example.studylog.service.oauth.TokenService;
 import org.example.studylog.util.CookieUtil;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Date;
+
 @RestController
 public class ReissueController {
 
     private final JWTUtil jwtUtil;
+    private final TokenService tokenService;
 
-    public ReissueController(JWTUtil jwtUtil) {
+    public ReissueController(JWTUtil jwtUtil, TokenService tokenService) {
         this.jwtUtil = jwtUtil;
+        this.tokenService = tokenService;
     }
 
     @PostMapping("/reissue")
@@ -32,23 +39,8 @@ public class ReissueController {
             }
         }
 
-        // refresh가 없을 때 BAD_REQUEST
-        if(refresh == null){
-            return new ResponseEntity<>("refresh token null", HttpStatus.BAD_REQUEST);
-        }
-
-        // refresh 토큰의 만료 여부 확인
-        try{
-            jwtUtil.isExpired(refresh);
-        } catch (ExpiredJwtException e){
-            return new ResponseEntity<>("refresh token expired", HttpStatus.BAD_REQUEST);
-        }
-
-        // 토큰이 refresh 인지 확인
-        String category = jwtUtil.getCategory(refresh);
-        if(!category.equals("refresh")){
-            return new ResponseEntity<>("invalid refresh token", HttpStatus.BAD_REQUEST);
-        }
+        // 리프레시 토큰 검증
+        tokenService.validateRefreshToken(refresh);
 
         // 새로운 access 토큰 발급
         String oauthId = jwtUtil.getOauthId(refresh);
@@ -57,9 +49,13 @@ public class ReissueController {
         String newAccess = jwtUtil.createJwt("access", oauthId, role, 600000L);
         String newRefresh = jwtUtil.createJwt("refresh", oauthId, role, 86400000L);
 
+        // Refresh DB에 기존의 Refresh 토큰 삭제 후 새로운 Refresh 토큰 저장
+        tokenService.replaceRefreshToken(refresh, newRefresh, oauthId, 86400000L);
+
         response.addCookie(CookieUtil.createCookie("access", newAccess));
         response.addCookie(CookieUtil.createCookie("refresh", newRefresh));
 
         return new ResponseEntity<>(HttpStatus.OK);
     }
+
 }
