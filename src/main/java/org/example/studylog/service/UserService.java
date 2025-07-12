dfd191c1fd36926d53b11d6702ed4836f357c847
정지윤 <samtori3849@gmail.com>
2025-07-12 20:29:41 +0900

    [Feat] 사용자 프로필 수정 메소드 updateUserProfile 추가
    프로필 이미지, 닉네임, 한줄 소개를 업데이트

--- src/main/java/org/example/studylog/service/UserService.java
+++ src/main/java/org/example/studylog/service/UserService.java
@@ -1,2 +1,35 @@
-package org.example.studylog.service;public class UserService {
+package org.example.studylog.service;
+
+import lombok.RequiredArgsConstructor;
+import org.example.studylog.dto.ProfileRequestDTO;
+import org.example.studylog.entity.user.User;
+import org.example.studylog.repository.UserRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+
+    private final UserRepository userRepository;
+    private final AwsS3Service awsS3Service;
+
+    public void updateUserProfile(ProfileRequestDTO request, String oauthId){
+        // 유저 찾기
+        User user = userRepository.findByOauthId(oauthId);
+
+        MultipartFile file = request.getProfileImage();
+        // S3 업로드
+        String imageUrl = awsS3Service.uploadFile(file);
+
+        // User 엔티티에 프로필 정보 업데이트
+        user.setNickname(request.getNickname());
+        user.setIntro(request.getIntro());
+        user.setProfileImage(imageUrl);
+
+        // 프로필 정보 업데이트 상태 바꾸기
+        user.setProfileCompleted(true);
+
+        userRepository.save(user);
+    }
 }
