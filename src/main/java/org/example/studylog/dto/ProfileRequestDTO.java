170a1e677afeab74e470bebd842365a184a0427d
정지윤 <samtori3849@gmail.com>
2025-07-12 20:27:10 +0900

    [Feat] 사용자 정보 받을 DTO 추가

--- src/main/java/org/example/studylog/dto/ProfileRequestDTO.java
+++ src/main/java/org/example/studylog/dto/ProfileRequestDTO.java
@@ -1,4 +1,13 @@
 package org.example.studylog.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.web.multipart.MultipartFile;
+
+@Getter
+@Setter
 public class ProfileRequestDTO {
+    private MultipartFile profileImage;
+    private String nickname;
+    private String intro;
 }
