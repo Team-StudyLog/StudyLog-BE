d631fca88c3d2012043054da71ca8e427c92fb1d
정지윤 <samtori3849@gmail.com>
2025-07-12 20:27:55 +0900

    [Feat] 사용자 프로필 수정 API 구현

--- src/main/java/org/example/studylog/controller/UserController.java
+++ src/main/java/org/example/studylog/controller/UserController.java
@@ -1,13 +1,33 @@
-package org.example.studylog.config;
+package org.example.studylog.controller;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.example.studylog.dto.ProfileRequestDTO;
+import org.example.studylog.service.UserService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/users")
 public class UserController {
 
+    private final UserService userService;
+
+    @PutMapping("/profile")
+    public ResponseEntity<?> updateProfile(@ModelAttribute ProfileRequestDTO request) {
+        // 로그인한 사용자 username 가져오기
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        String oauthId = auth.getName();
+
+        userService.updateUserProfile(request, oauthId);
+        return ResponseEntity.ok("프로필 업데이트 완료");
+    }
 
 }
