b1abc4f9382bbb89460d9c56da157b35838d7a5a
정지윤 <samtori3849@gmail.com>
2025-07-12 20:25:37 +0900

    [Feat] 토큰 재발급 경로 /auth/**를 permitAll로 설정

--- src/main/java/org/example/studylog/config/SecurityConfig.java
+++ src/main/java/org/example/studylog/config/SecurityConfig.java
@@ -71,7 +71,7 @@ public class SecurityConfig {
         // 경로별 인가 작업
         http
                 .authorizeHttpRequests((auth) -> auth
-                        .requestMatchers("/", "/login", "/reissue").permitAll()
+                        .requestMatchers("/", "/login", "/auth/**").permitAll()
                         .anyRequest().authenticated());
 
         // 세션 설정 : STATELESS
