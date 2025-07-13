58b714441f475b75e96affbef80fb1252d544dd3
정지윤 <samtori3849@gmail.com>
2025-07-13 17:17:51 +0900

    [Feat] 프로필 정보 응답 DTO 생성

--- src/main/java/org/example/studylog/dto/ProfileResponseDTO.java
+++ src/main/java/org/example/studylog/dto/ProfileResponseDTO.java
@@ -1,2 +1,14 @@
-package org.example.studylog.dto;public class PorfileResponseDTO {
+package org.example.studylog.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+@Builder
+@AllArgsConstructor
+@Getter
+public class ProfileResponseDTO {
+    private String profileImage;
+    private String nickname;
+    private String intro;
 }
