3f5a632684f32f86903d4a2fba507c616f184f14
정지윤 <samtori3849@gmail.com>
2025-07-13 18:01:28 +0900

    [Feat] 인증 없이 허용 경로에 /auth/** 다시 추가

--- src/main/java/org/example/studylog/config/SecurityConfig.java
+++ src/main/java/org/example/studylog/config/SecurityConfig.java
@@ -71,14 +71,13 @@ public class SecurityConfig {
         // 경로별 인가 작업
         http
                 .authorizeHttpRequests((auth) -> auth
-                        .requestMatchers("/", "/login").permitAll()
+                        .requestMatchers("/", "/login", "/auth/**").permitAll()
                         .anyRequest().authenticated());
 
         // 세션 설정 : STATELESS
         http
                 .sessionManagement((session) -> session
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
-
 
         return http.build();
     }
