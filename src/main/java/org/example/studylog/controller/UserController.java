7cb631e7f48d8470fa36ed3827d78ad1758fbd28
정지윤 <samtori3849@gmail.com>
2025-07-15 22:38:17 +0900

    [Feat] Swagger 적용

--- src/main/java/org/example/studylog/controller/UserController.java
+++ src/main/java/org/example/studylog/controller/UserController.java
@@ -1,9 +1,14 @@
 package org.example.studylog.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.example.studylog.dto.ProfileRequestDTO;
 import org.example.studylog.dto.ProfileResponseDTO;
+import org.example.studylog.dto.ResponseDTO;
 import org.example.studylog.service.UserService;
 import org.example.studylog.util.ResponseUtil;
 import org.springframework.http.ResponseEntity;
@@ -19,6 +24,7 @@ public class UserController {
 
     private final UserService userService;
 
+    @Operation(summary = "프로필 업데이트 api", description = "프로필 추가 및 수정을 위한 api")
     @PutMapping("/profile")
     public ResponseEntity<?> updateProfile(@ModelAttribute ProfileRequestDTO request) {
         // 로그인한 사용자 oauthId 가져오기
@@ -29,7 +35,10 @@ public class UserController {
         return ResponseUtil.buildResponse(200, "사용자 프로필 업데이트 완료", dto);
     }
 
+    @Operation(summary = "프로필 조회 api")
     @GetMapping("/profile")
+    @ApiResponse(responseCode = "200", description = "성공 시 data 필드는 다음과 같습니다",
+        content = @Content(schema = @Schema(implementation = ProfileResponseDTO.class)))
     public ResponseEntity<?> getProfile(){
         // 로그인한 사용자 oauthId 가져오기
         Authentication auth = SecurityContextHolder.getContext().getAuthentication();
