860df882fa4c317c23cf99c4c7b59358d40d24d7
정지윤 <samtori3849@gmail.com>
2025-07-16 23:25:50 +0900

    [Feat] Category 엔티티 추가

--- src/main/java/org/example/studylog/entity/category/Category.java
+++ src/main/java/org/example/studylog/entity/category/Category.java
@@ -1,4 +1,4 @@
-package org.example.studylog.entity;
+package org.example.studylog.entity.category;
 
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
@@ -6,6 +6,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.experimental.SuperBuilder;
+import org.example.studylog.entity.StudyRecord;
 import org.example.studylog.entity.quiz.Quiz;
 import org.example.studylog.entity.user.User;
 
@@ -27,9 +28,10 @@ public class Category {
     @Column(nullable = false, length = 10)
     private String name;
 
-    
+    @Enumerated(EnumType.STRING)
+    private Color color;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id", nullable = false)
     private User user;
 
