14f4fb281e1a3aadc9e27887861a2a2b655809ad
정지윤 <samtori3849@gmail.com>
2025-07-18 01:09:08 +0900

    [Feat] User와 Friend가 존재 여부 반환하는 메소드 추가

---
+++ src/main/java/org/example/studylog/exception/ErrorCode.java
@@ -0,0 +1,2 @@
+package org.example.studylog.exception;public enum ErrorCode {
+}
--- src/main/java/org/example/studylog/repository/FriendRepository.java
+++ src/main/java/org/example/studylog/repository/FriendRepository.java
@@ -1,2 +1,11 @@
-package org.example.studylog.repository;public interface FriendRepository {
+package org.example.studylog.repository;
+
+
+import org.example.studylog.entity.Friend;
+import org.example.studylog.entity.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FriendRepository extends JpaRepository<Friend, Long> {
+
+    boolean existsByUserAndFriend(User user, User friend);
 }
