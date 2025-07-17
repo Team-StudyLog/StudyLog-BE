8209019a3ff1dbeb68b5a891c2e2222632db3298
정지윤 <samtori3849@gmail.com>
2025-07-18 01:10:06 +0900

    [Feat] 커스텀 예외 ErrorCode 클래스 추가

--- src/main/java/org/example/studylog/exception/ErrorCode.java
+++ src/main/java/org/example/studylog/exception/ErrorCode.java
@@ -1,2 +1,18 @@
-package org.example.studylog.exception;public enum ErrorCode {
+package org.example.studylog.exception;
+
+import lombok.Getter;
+
+@Getter
+public enum ErrorCode {
+    USER_CODE_NOT_FOUND(404,"코드에 해당하는 사용자가 없습니다."),
+    ALREADY_FRIEND(400,"이미 친구입니다."),
+    ;
+
+    private int status;
+    private final String message;
+
+    ErrorCode(int status, String message){
+        this.status = status;
+        this.message = message;
+    }
 }
