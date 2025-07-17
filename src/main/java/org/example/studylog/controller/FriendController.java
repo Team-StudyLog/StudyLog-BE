3a8f84278d6b377f4aa6f4877755baa3ccb8b850
정지윤 <samtori3849@gmail.com>
2025-07-17 21:17:59 +0900

    [Feat] 코드로 사용자 이름 조회 API 추가

--- src/main/java/org/example/studylog/controller/FriendController.java
+++ src/main/java/org/example/studylog/controller/FriendController.java
@@ -1,2 +1,27 @@
-package org.example.studylog.controller;public class FriendController {
+package org.example.studylog.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.example.studylog.dto.friend.FriendNameDTO;
+import org.example.studylog.service.FriendService;
+import org.example.studylog.util.ResponseUtil;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/friends")
+public class FriendController {
+
+    private final FriendService friendService;
+
+    @GetMapping(params = "code")
+    public ResponseEntity<?> findUserByCode(@RequestParam String code) {
+        FriendNameDTO dto = friendService.findUserByCode(code);
+        return ResponseUtil.buildResponse(200, "사용자 이름 조회 완료", dto);
+    }
 }
--- src/main/java/org/example/studylog/service/FriendService.java
+++ src/main/java/org/example/studylog/service/FriendService.java
@@ -1,2 +1,25 @@
-package org.example.studylog.service;public class FriendService {
+package org.example.studylog.service;
+
+import lombok.RequiredArgsConstructor;
+import org.example.studylog.dto.friend.FriendNameDTO;
+import org.example.studylog.entity.user.User;
+import org.example.studylog.exception.UserNotFoundException;
+import org.example.studylog.repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class FriendService {
+
+    private final UserRepository userRepository;
+
+    public FriendNameDTO findUserByCode(String code) {
+        // 코드로 사용자 조회
+        User user = userRepository.findByCode(code)
+                .orElseThrow(() -> new UserNotFoundException("코드에 해당하는 사용자 없음"));
+
+        return FriendNameDTO.builder()
+                .nickname(user.getNickname())
+                .build();
+    }
 }
