91c1897efc5d0347faee26a05c6d1da03e018ddc
정지윤 <samtori3849@gmail.com>
2025-07-18 01:12:32 +0900

    [Feat] 친구 추가 API

--- src/main/java/org/example/studylog/controller/FriendController.java
+++ src/main/java/org/example/studylog/controller/FriendController.java
@@ -1,16 +1,17 @@
 package org.example.studylog.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.example.studylog.dto.friend.FriendNameDTO;
+import org.example.studylog.dto.friend.FriendRequestDTO;
 import org.example.studylog.service.FriendService;
 import org.example.studylog.util.ResponseUtil;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RestController
@@ -26,4 +27,16 @@ public class FriendController {
         FriendNameDTO dto = friendService.findUserByCode(code);
         return ResponseUtil.buildResponse(200, "사용자 이름 조회 완료", dto);
     }
+
+    @Operation(summary = "code로 친구 추가", description = "코드로 친구 추가 API")
+    @PostMapping
+    public ResponseEntity<?> addFriend(@RequestBody @Valid FriendRequestDTO request) {
+        // 로그인한 사용자 oauthId 가져오기
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        String oauthId = auth.getName();
+
+        friendService.addFriend(request, oauthId);
+        return ResponseUtil.buildResponse(201, "친구 추가 완료", null);
+    }
 }
+
--- src/main/java/org/example/studylog/service/FriendService.java
+++ src/main/java/org/example/studylog/service/FriendService.java
@@ -1,9 +1,15 @@
 package org.example.studylog.service;
 
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.example.studylog.dto.friend.FriendNameDTO;
+import org.example.studylog.dto.friend.FriendRequestDTO;
+import org.example.studylog.entity.Friend;
 import org.example.studylog.entity.user.User;
+import org.example.studylog.exception.BusinessException;
+import org.example.studylog.exception.ErrorCode;
 import org.example.studylog.exception.UserNotFoundException;
+import org.example.studylog.repository.FriendRepository;
 import org.example.studylog.repository.UserRepository;
 import org.springframework.stereotype.Service;
 
@@ -12,6 +18,7 @@ import org.springframework.stereotype.Service;
 public class FriendService {
 
     private final UserRepository userRepository;
+    private final FriendRepository friendRepository;
 
     public FriendNameDTO findUserByCode(String code) {
         // 코드로 사용자 조회
@@ -21,5 +28,33 @@ public class FriendService {
         return FriendNameDTO.builder()
                 .nickname(user.getNickname())
                 .build();
+    }
+
+    @Transactional
+    public void addFriend(FriendRequestDTO request, String oauthId) {
+        // 로그인한 유저 찾기
+        User user = userRepository.findByOauthId(oauthId);
+        // 친구할 유저 찾기
+        User friend = userRepository.findByCode(request.getCode())
+                .orElseThrow(() -> new BusinessException(ErrorCode.USER_CODE_NOT_FOUND));
+
+        // 이미 친구인지 확인
+        boolean alreadyAdded = friendRepository.existsByUserAndFriend(user, friend);
+        if(alreadyAdded){
+            throw new BusinessException(ErrorCode.ALREADY_FRIEND);
+        }
+
+        // 단방향 관계 2개 설정 (양방향)
+        Friend toFriend = Friend.builder()
+                .user(user)
+                .friend(friend)
+                .build();
+        Friend fromFriend = Friend.builder()
+                .user(friend)
+                .friend(user)
+                .build();
+
+        friendRepository.save(toFriend);
+        friendRepository.save(fromFriend);
     }
 }
