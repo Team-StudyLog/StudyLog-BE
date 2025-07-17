7136582023b4a9821e49723b73ebf33eaa7f07f6
정지윤 <samtori3849@gmail.com>
2025-07-17 21:15:55 +0900

    [Feat] 코드로 사용자를 조회하는 메소드 추가
    사용자 반환 시, Optional로 객체로 반환함

--- src/main/java/org/example/studylog/repository/UserRepository.java
+++ src/main/java/org/example/studylog/repository/UserRepository.java
@@ -3,9 +3,13 @@ package org.example.studylog.repository;
 import org.example.studylog.entity.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
 
     User findByOauthId(String oauthId);
 
     boolean existsByCode(String code);
+
+    Optional<User> findByCode(String code);
 }
