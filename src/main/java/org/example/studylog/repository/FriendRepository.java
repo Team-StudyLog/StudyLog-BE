f6d902fd8ee06e123d3aa7124c4fb38b9eba63a7
정지윤 <samtori3849@gmail.com>
2025-07-18 16:15:05 +0900

    [Feat] findByUserAndFriend 메소드 추가

--- src/main/java/org/example/studylog/repository/FriendRepository.java
+++ src/main/java/org/example/studylog/repository/FriendRepository.java
@@ -5,7 +5,11 @@ import org.example.studylog.entity.Friend;
 import org.example.studylog.entity.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface FriendRepository extends JpaRepository<Friend, Long> {
 
     boolean existsByUserAndFriend(User user, User friend);
+
+    Optional<Friend> findByUserAndFriend(User user, User friend);
 }
