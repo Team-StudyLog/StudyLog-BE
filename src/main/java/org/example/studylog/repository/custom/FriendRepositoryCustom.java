f54874ea1dae786d4b2b4c422de42d027b9d6cc9
정지윤 <samtori3849@gmail.com>
2025-07-18 14:29:55 +0900

    [Feat] QueryDsl로 사용자의 친구 목록 불러오는 메소드 추가

--- src/main/java/org/example/studylog/repository/custom/FriendRepositoryCustom.java
+++ src/main/java/org/example/studylog/repository/custom/FriendRepositoryCustom.java
@@ -1,2 +1,10 @@
-package org.example.studylog.repository.custom;public interface FriendRepositoryCustom {
+package org.example.studylog.repository.custom;
+
+import org.example.studylog.dto.friend.FriendResponseDTO;
+import org.example.studylog.entity.user.User;
+
+import java.util.List;
+
+public interface FriendRepositoryCustom {
+    List<FriendResponseDTO> findFriendListByUser(User user);
 }
--- src/main/java/org/example/studylog/repository/custom/FriendRepositoryImpl.java
+++ src/main/java/org/example/studylog/repository/custom/FriendRepositoryImpl.java
@@ -1,2 +1,33 @@
-package org.example.studylog.repository.custom;public class FriendRepositoryImpl {
+package org.example.studylog.repository.custom;
+
+import com.querydsl.core.types.Projections;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import lombok.RequiredArgsConstructor;
+import org.example.studylog.dto.friend.FriendResponseDTO;
+import org.example.studylog.entity.QFriend;
+import org.example.studylog.entity.user.QUser;
+import org.example.studylog.entity.user.User;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class FriendRepositoryImpl implements FriendRepositoryCustom{
+
+    private final JPAQueryFactory queryFactory;
+
+    @Override
+    public List<FriendResponseDTO> findFriendListByUser(User user) {
+        QFriend friend = QFriend.friend1;
+
+        return queryFactory
+                .select(Projections.constructor(FriendResponseDTO.class,
+                        friend.friend.id,
+                        friend.friend.nickname,
+                        friend.friend.profileImage))
+                .from(friend)
+                .where(friend.user.eq(user))
+                .fetch();
+    }
 }
