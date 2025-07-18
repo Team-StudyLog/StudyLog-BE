f5964a52679a72537335ab52240bc6a9aab25bdd
정지윤 <samtori3849@gmail.com>
2025-07-18 14:31:03 +0900

    [Feat] 친구 목록 조회 API 추가

--- src/main/java/org/example/studylog/controller/FriendController.java
+++ src/main/java/org/example/studylog/controller/FriendController.java
@@ -6,12 +6,15 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.example.studylog.dto.friend.FriendNameDTO;
 import org.example.studylog.dto.friend.FriendRequestDTO;
+import org.example.studylog.dto.friend.FriendResponseDTO;
 import org.example.studylog.service.FriendService;
 import org.example.studylog.util.ResponseUtil;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Slf4j
 @RestController
@@ -26,6 +29,17 @@ public class FriendController {
     public ResponseEntity<?> findUserByCode(@RequestParam String code) {
         FriendNameDTO dto = friendService.findUserByCode(code);
         return ResponseUtil.buildResponse(200, "사용자 이름 조회 완료", dto);
+    }
+
+    @Operation(summary = "친구 목록 조회", description = "로그인한 사용자의 친구 목록 조회 API")
+    @GetMapping
+    public ResponseEntity<?> getFriendList(){
+        // 로그인한 사용자 oauthId 가져오기
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        String oauthId = auth.getName();
+
+        List<FriendResponseDTO> friends = friendService.getFriendList(oauthId);
+        return ResponseUtil.buildResponse(200, "친구 목록 조회 완료", friends);
     }
 
     @Operation(summary = "code로 친구 추가", description = "코드로 친구 추가 API")
--- src/main/java/org/example/studylog/service/FriendService.java
+++ src/main/java/org/example/studylog/service/FriendService.java
@@ -1,17 +1,20 @@
 package org.example.studylog.service;
 
-import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.example.studylog.dto.friend.FriendNameDTO;
 import org.example.studylog.dto.friend.FriendRequestDTO;
+import org.example.studylog.dto.friend.FriendResponseDTO;
 import org.example.studylog.entity.Friend;
 import org.example.studylog.entity.user.User;
 import org.example.studylog.exception.BusinessException;
 import org.example.studylog.exception.ErrorCode;
-import org.example.studylog.exception.UserNotFoundException;
 import org.example.studylog.repository.FriendRepository;
 import org.example.studylog.repository.UserRepository;
+import org.example.studylog.repository.custom.FriendRepositoryImpl;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -19,7 +22,9 @@ public class FriendService {
 
     private final UserRepository userRepository;
     private final FriendRepository friendRepository;
+    private final FriendRepositoryImpl friendRepositoryImpl;
 
+    @Transactional(readOnly = true)
     public FriendNameDTO findUserByCode(String code) {
         // 코드로 사용자 조회
         User user = userRepository.findByCode(code)
@@ -28,6 +33,14 @@ public class FriendService {
         return FriendNameDTO.builder()
                 .nickname(user.getNickname())
                 .build();
+    }
+
+    @Transactional(readOnly = true)
+    public List<FriendResponseDTO> getFriendList(String oauthId) {
+        // 로그인한 유저 찾기
+        User user = userRepository.findByOauthId(oauthId);
+
+        return friendRepositoryImpl.findFriendListByUser(user);
     }
 
     @Transactional
@@ -57,4 +70,6 @@ public class FriendService {
         friendRepository.save(toFriend);
         friendRepository.save(fromFriend);
     }
+
+
 }
