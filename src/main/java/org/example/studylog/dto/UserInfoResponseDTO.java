7808d1b35d6ab9b1a72228482f330060de8fe6f7
정지윤 <samtori3849@gmail.com>
2025-07-18 17:12:52 +0900

    [Feat] 유저 정보 반환하는 UserInfoResponseDTO 추가

--- src/main/java/org/example/studylog/dto/UserInfoResponseDTO.java
+++ src/main/java/org/example/studylog/dto/UserInfoResponseDTO.java
@@ -1,5 +1,18 @@
 package org.example.studylog.dto;
 
-public class UserPageDTO {
-    
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class UserInfoResponseDTO {
+    private String profileImage;
+    private String nickname;
+    private String intro;
+    private Long friendCount;
+    private String code;
 }
