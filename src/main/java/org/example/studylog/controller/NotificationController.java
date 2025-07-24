6af1c9e4c55a6812cc08a971769a20471b79368d
정지윤 <samtori3849@gmail.com>
2025-07-24 13:04:46 +0900

    [Feat] 알림 구독/전송 기능 및 알림 목록 조회 기능 추가

--- src/main/java/org/example/studylog/controller/NotificationController.java
+++ src/main/java/org/example/studylog/controller/NotificationController.java
@@ -1,17 +1,22 @@
 package org.example.studylog.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.example.studylog.dto.notification.NotificationListResponseDTO;
 import org.example.studylog.service.NotificationService;
+import org.example.studylog.util.ResponseUtil;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
+import java.util.List;
+
 @RestController
 @RequiredArgsConstructor
-public class SseController {
+public class NotificationController {
 
     private final NotificationService notificationService;
 
@@ -24,7 +29,14 @@ public class SseController {
         return notificationService.createEmitter(oauthId);
     }
 
+    @GetMapping("/notifications")
+    public ResponseEntity<?> getNotificationList() {
+        // 로그인한 사용자 oauthId 가져오기
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        String oauthId = auth.getName();
 
-
+        List<NotificationListResponseDTO> list = notificationService.getNotificationList(oauthId);
+        return ResponseUtil.buildResponse(200, "알림 목록 조회 완료", list);
+    }
 
 }
