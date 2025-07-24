23cafbd306640064b9ee7055988c64df55e89833
정지윤 <samtori3849@gmail.com>
2025-07-24 12:51:53 +0900

    [Feat] SseEmitter 객체를 저장하는 레포지토리 추가

--- src/main/java/org/example/studylog/repository/EmitterRepository.java
+++ src/main/java/org/example/studylog/repository/EmitterRepository.java
@@ -1,2 +1,31 @@
-package org.example.studylog.repository;public class EmitterRepository {
+package org.example.studylog.repository;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Repository;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+
+@Slf4j
+@Repository
+public class EmitterRepository {
+    private Map<String, SseEmitter> emitterMap = new HashMap<>();
+
+    public SseEmitter save(String oauthId, SseEmitter sseEmitter){
+        emitterMap.put(oauthId, sseEmitter);
+        log.info("Saved SseEmitter for {}", sseEmitter);
+        return sseEmitter;
+    }
+
+    public SseEmitter get(String oauthId){
+        log.info("Got SseEmitter for {}", oauthId);
+        return emitterMap.get(oauthId);
+    }
+
+    public void delete(String oauthId) {
+        emitterMap.remove(oauthId);
+        log.info("Deleted SseEmitter for {}", oauthId);
+    }
 }
