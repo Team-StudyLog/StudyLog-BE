a1fe8cbbe9b804f343a220f877ae37cf64bae5fb
정지윤 <samtori3849@gmail.com>
2025-07-24 12:52:32 +0900

    [Feat] NotificationRepository 추가

--- src/main/java/org/example/studylog/repository/NotificationRepository.java
+++ src/main/java/org/example/studylog/repository/NotificationRepository.java
@@ -1,2 +1,13 @@
-package org.example.studylog.repository;public interface NotificationRepository {
+package org.example.studylog.repository;
+
+import org.example.studylog.entity.notification.Notification;
+import org.example.studylog.entity.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface NotificationRepository extends JpaRepository<Notification, Long> {
+
+    List<Notification> findTop30ByUserOrderByCreatedAtDesc(User user);
+
 }
