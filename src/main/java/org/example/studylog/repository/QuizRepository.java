3e68644fb7006206f07144f3bf1061c43dc3c4b6
정지윤 <samtori3849@gmail.com>
2025-07-27 20:38:48 +0900

    [Feat] QuizRepository 추가

--- src/main/java/org/example/studylog/repository/QuizRepository.java
+++ src/main/java/org/example/studylog/repository/QuizRepository.java
@@ -1,2 +1,8 @@
-package org.example.studylog.repository;public interface QuizRepository {
+package org.example.studylog.repository;
+
+import org.example.studylog.entity.quiz.Quiz;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface QuizRepository extends JpaRepository<Quiz, Long> {
 }
