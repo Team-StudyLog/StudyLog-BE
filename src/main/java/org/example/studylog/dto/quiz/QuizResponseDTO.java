79718283dd03a5c7629c0b000f876a19009625b2
정지윤 <samtori3849@gmail.com>
2025-07-27 20:49:38 +0900

    [Feat] 퀴즈 응답 DTO 추가

--- src/main/java/org/example/studylog/dto/quiz/QuizResponseDTO.java
+++ src/main/java/org/example/studylog/dto/quiz/QuizResponseDTO.java
@@ -1,2 +1,18 @@
-package org.example.studylog.dto.quiz;public class QuizResponseDTO {
+package org.example.studylog.dto.quiz;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.example.studylog.dto.CategoryDTO;
+
+@Setter
+@Getter
+public class QuizResponseDTO {
+
+    private String createdAt;
+    private String type;
+    private CategoryDTO category;
+    private String question;
+    private String answer;
+    private String level;
+    private Long recordId;
 }
