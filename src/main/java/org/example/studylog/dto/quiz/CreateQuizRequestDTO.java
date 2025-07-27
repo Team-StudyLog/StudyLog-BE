cbcd7881f71efbee476a9d15acec233ad5c43b93
정지윤 <samtori3849@gmail.com>
2025-07-27 20:49:19 +0900

    [Feat] 퀴즈 생성 요청 DTO 추가

--- src/main/java/org/example/studylog/dto/quiz/CreateQuizRequestDTO.java
+++ src/main/java/org/example/studylog/dto/quiz/CreateQuizRequestDTO.java
@@ -1,2 +1,21 @@
-package org.example.studylog.dto.quiz;public class CreateQuizRequestDTO {
+package org.example.studylog.dto.quiz;
+
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
+import org.example.studylog.entity.quiz.QuizLevel;
+
+@Getter
+@Setter
+public class CreateQuizRequestDTO {
+
+    @NotNull(message = "난이도는 필수입니다")
+    private QuizLevel level;
+
+    @Min(value = 1, message = "퀴즈 갯수는 1 이상이어야 합니다")
+    private int quizCount;
+
+    private String requirement;
 }
