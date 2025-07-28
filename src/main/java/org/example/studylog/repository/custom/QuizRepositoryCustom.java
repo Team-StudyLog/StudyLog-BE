13d32e560cbf257aa37ce90fcf5f5a55a181545e
정지윤 <samtori3849@gmail.com>
2025-07-28 11:16:59 +0900

    [Feat] findQuizzes 메소드 추가
    퀴즈 목록 필터링 조건에 맞는 동적 쿼리 생성

--- src/main/java/org/example/studylog/repository/custom/QuizRepositoryCustom.java
+++ src/main/java/org/example/studylog/repository/custom/QuizRepositoryCustom.java
@@ -1,4 +1,10 @@
 package org.example.studylog.repository.custom;
 
-public interface getQuizList {
+import org.example.studylog.entity.quiz.Quiz;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public interface QuizRepositoryCustom {
+    List<Quiz> findQuizzes(Long userId, Long lastId, int size, LocalDate date, Long categoryId, String query);
 }
--- src/main/java/org/example/studylog/repository/custom/QuizRepositoryImpl.java
+++ src/main/java/org/example/studylog/repository/custom/QuizRepositoryImpl.java
@@ -1,2 +1,54 @@
-package org.example.studylog.repository.custom;public class QuizRepositoryImpl {
+package org.example.studylog.repository.custom;
+
+import com.querydsl.core.BooleanBuilder;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import lombok.RequiredArgsConstructor;
+import org.example.studylog.entity.category.QCategory;
+import org.example.studylog.entity.quiz.QQuiz;
+import org.example.studylog.entity.quiz.Quiz;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@RequiredArgsConstructor
+public class QuizRepositoryImpl implements QuizRepositoryCustom {
+
+    private final JPAQueryFactory queryFactory;
+
+    @Override
+    public List<Quiz> findQuizzes(Long userId, Long lastId, int size, LocalDate date, Long categoryId, String query) {
+
+        QQuiz quiz = QQuiz.quiz;
+
+        BooleanBuilder builder = new BooleanBuilder();
+        builder.and(quiz.user.id.eq(userId));
+
+        if (lastId != null){
+            builder.and(quiz.id.lt(lastId));
+        }
+
+        if (date != null){
+            builder.and(quiz.createdAt.between(
+                    date.atStartOfDay(),
+                    date.plusDays(1).atStartOfDay()
+            ));
+        }
+
+        if (categoryId != null){
+            builder.and(quiz.category.id.eq(categoryId));
+        }
+
+        if (query != null && !query.isBlank()) {
+            builder.and(
+                    quiz.question.containsIgnoreCase(query));
+        }
+
+        return queryFactory.selectFrom(quiz)
+                .join(quiz.category, QCategory.category).fetchJoin()
+                .where(builder)
+                .orderBy(quiz.id.desc())
+                .limit(size)
+                .fetch();
+    }
+
 }
