da0e81c15b99e841d87f566b3caad2d42c32b585
정지윤 <samtori3849@gmail.com>
2025-08-01 23:04:04 +0900

    [Feat] 레벨 증감 이벤트 클래스 추가

--- src/main/java/org/example/studylog/event/LevelEvent.java
+++ src/main/java/org/example/studylog/event/LevelEvent.java
@@ -1,2 +1,18 @@
-package org.example.studylog.event;public class LevelUpEvent {
+package org.example.studylog.event;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.example.studylog.entity.user.User;
+
+@Getter
+@AllArgsConstructor
+public class LevelEvent {
+
+    private final User user;
+    private final int newLevel;
+    private final ActionType action;
+
+    public enum ActionType{
+        UP, DOWN
+    }
 }
--- src/main/java/org/example/studylog/event/RecordEvent.java
+++ src/main/java/org/example/studylog/event/RecordEvent.java
@@ -1,15 +1,13 @@
 package org.example.studylog.event;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 import org.example.studylog.entity.user.User;
 
-public class RecordCreatedEvent {
+@Getter
+@AllArgsConstructor
+public class RecordEvent {
+
     private final User user;
 
-    public RecordCreatedEvent(User user) {
-        this.user = user;
-    }
-
-    public User getUser() {
-        return user;
-    }
 }
