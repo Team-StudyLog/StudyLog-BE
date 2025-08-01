80cbfb74d72282e32dbac625bcde7b595ea7a075
정지윤 <samtori3849@gmail.com>
2025-08-01 23:27:07 +0900

    [Feat] 사용자의 Level을 변경하는 LevelChangeListener 추가

--- src/main/java/org/example/studylog/event/listener/LevelChangeListener.java
+++ src/main/java/org/example/studylog/event/listener/LevelChangeListener.java
@@ -1,6 +1,10 @@
 package org.example.studylog.event.listener;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.example.studylog.entity.user.LevelThresholds;
+import org.example.studylog.entity.user.User;
+import org.example.studylog.event.LevelEvent;
 import org.example.studylog.event.RecordEvent;
 import org.example.studylog.repository.UserRepository;
 import org.springframework.context.ApplicationEventPublisher;
@@ -9,8 +13,9 @@ import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
 @Component
+@Slf4j
 @RequiredArgsConstructor
-public class LevelUpListener {
+public class LevelChangeListener {
 
     private final UserRepository userRepository;
     private final ApplicationEventPublisher eventPublisher;
@@ -18,14 +23,23 @@ public class LevelUpListener {
     @EventListener
     @Transactional
     public void handleRecordEvent(RecordEvent event) {
-        
+        User currentUser = event.getUser();
+        long recordCount = currentUser.getRecordCount();
 
-        if(event.getAction() == RecordEvent.ActionType.CREATED){
-            // 레벨 체크 (기록 생성 시)
+        log.info("레벨 체크 시작: USER = {}, CURRENT_LEVEL = {}, RECORD_COUNT= {}",
+                currentUser.getOauthId(), currentUser.getLevel(), recordCount);
 
-        }
-        else if(event.getAction() == RecordEvent.ActionType.DELETED){
-            // 레벨 체크 (기록 삭제 시)
+        int lastLevel = currentUser.getLevel();
+        int newLevel = LevelThresholds.getLevelForRecordCount(recordCount);
+        if(lastLevel != newLevel){
+            currentUser.setLevel(newLevel); // 레벨 업데이트
+
+            if(lastLevel > newLevel){
+                eventPublisher.publishEvent(new LevelEvent(currentUser, newLevel, LevelEvent.ActionType.DOWN));
+            }
+            else if(lastLevel < newLevel){
+                eventPublisher.publishEvent(new LevelEvent(currentUser, newLevel, LevelEvent.ActionType.UP));
+            }
         }
     }
 }
