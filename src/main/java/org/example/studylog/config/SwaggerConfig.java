fc388e9d272007774d0f3792e3af9e8d30ba1694
정지윤 <samtori3849@gmail.com>
2025-08-07 11:13:14 +0900

    [Feat] Swagger에 Authorization 헤더 적용

--- src/main/java/org/example/studylog/config/SwaggerConfig.java
+++ src/main/java/org/example/studylog/config/SwaggerConfig.java
@@ -1,5 +1,7 @@
 package org.example.studylog.config;
 
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
@@ -10,8 +12,15 @@ import org.springframework.context.annotation.Configuration;
 public class SwaggerConfig {
     @Bean
     public OpenAPI openAPI(){
+        SecurityScheme securityScheme = new SecurityScheme()
+                .type(SecurityScheme.Type.HTTP)
+                .scheme("bearer")
+                .bearerFormat("JWT");
+        SecurityRequirement securityRequirement = new SecurityRequirement().addList("bearerAuth");
+
         return new OpenAPI()
-                .components(new Components())
+                .components(new Components().addSecuritySchemes("bearerAuth", securityScheme))
+                .addSecurityItem(securityRequirement)
                 .info(apiInfo());
     }
 
