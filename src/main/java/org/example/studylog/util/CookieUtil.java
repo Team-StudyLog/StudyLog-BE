0c194febc5378a30850d8f80379d3da029af617c
정지윤 <samtori3849@gmail.com>
2025-08-17 10:41:28 +0900

    [Feat] 쿠키에 secure, domain, sameSite 옵션 추가

--- src/main/java/org/example/studylog/util/CookieUtil.java
+++ src/main/java/org/example/studylog/util/CookieUtil.java
@@ -8,9 +8,9 @@ public class CookieUtil {
                 .httpOnly(true)              // JS 접근 불가
                 .path("/")                   // 모든 경로에서 쿠키 전송
                 .maxAge(60 * 60 * 60)        // 유효 시간 (초 단위)
-//                .secure(true)                // HTTPS에서만 전송
-//                .domain(".studylog.shop")    // 도메인 지정 (서브도메인 포함)
-//                .sameSite("None")            // 크로스 도메인 쿠키 허용 시 필요
+                .secure(true)                // HTTPS에서만 전송
+                .domain(".studylog.shop")    // 도메인 지정 (서브도메인 포함)
+                .sameSite("None")            // 크로스 도메인 쿠키 허용 시 필요
                 .build();
     }
 }
