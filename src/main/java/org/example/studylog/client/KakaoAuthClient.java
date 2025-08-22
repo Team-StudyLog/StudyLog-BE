a512edc1e24fd667a48f03efc1348e2525698cf9
정지윤 <samtori3849@gmail.com>
2025-08-22 20:06:51 +0900

    [Feat] 카카오 연동 해제 및 accessToken 재발급 client 추가
    KakaoAuthClient: accessToken 재발급 client
    KakaoApiClient: 연동 해제 client

--- src/main/java/org/example/studylog/client/KakaoAuthClient.java
+++ src/main/java/org/example/studylog/client/KakaoAuthClient.java
@@ -1,4 +1,18 @@
 package org.example.studylog.client;
 
-public interface KakaoClient {
+import com.nimbusds.oauth2.sdk.TokenResponse;
+import org.example.studylog.dto.oauth.OAuthTokenResponse;
+import org.springframework.http.MediaType;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.service.annotation.HttpExchange;
+import org.springframework.web.service.annotation.PostExchange;
+
+@HttpExchange(accept = MediaType.APPLICATION_JSON_VALUE)
+public interface KakaoAuthClient {
+
+    // POST /oauth/token  (x-www-form-urlencoded)
+    @PostExchange(url = "/oauth/token",
+            contentType = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+    OAuthTokenResponse refreshToken(@RequestBody MultiValueMap<String, String> form);
 }
