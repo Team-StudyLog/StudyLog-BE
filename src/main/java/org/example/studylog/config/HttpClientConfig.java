2eb36b5dcf59de4404e9764696bd278634c226bf
정지윤 <samtori3849@gmail.com>
2025-08-22 19:49:44 +0900

    [Feat] GoogleClient, KakaoAuthClient, KakaoApiClient 추가
    GoogleClient: 구글 OAuth 토큰 관리
    KakaoAuthClient: 카카오 토큰 관리
    KakaoApiClient: 카카오 사용자/리소스 API 호출(언링크 등)

--- src/main/java/org/example/studylog/config/HttpClientConfig.java
+++ src/main/java/org/example/studylog/config/HttpClientConfig.java
@@ -1,9 +1,14 @@
 package org.example.studylog.config;
 
 import org.example.studylog.client.ChatGptClient;
+import org.example.studylog.client.GoogleClient;
+import org.example.studylog.client.KakaoApiClient;
+import org.example.studylog.client.KakaoAuthClient;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.web.client.RestClient;
 import org.springframework.web.client.support.RestClientAdapter;
 import org.springframework.web.service.invoker.HttpServiceProxyFactory;
@@ -26,5 +31,42 @@ public class HttpClientConfig {
         HttpServiceProxyFactory factory = HttpServiceProxyFactory.builderFor(adapter).build();
 
         return factory.createClient(ChatGptClient.class);
+    }
+
+    @Bean
+    public GoogleClient googleClient(){
+        RestClient restClient = RestClient.builder()
+                .baseUrl("https://oauth2.googleapis.com")
+                .defaultHeader(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON_VALUE)
+                .build();
+
+        HttpServiceProxyFactory factory = HttpServiceProxyFactory
+                .builderFor(RestClientAdapter.create(restClient))
+                .build();
+
+        return factory.createClient(GoogleClient.class);
+
+    }
+
+    @Bean
+    public KakaoAuthClient kakaoAuthClient(RestClient.Builder builder) {
+        RestClient rc = builder
+                .baseUrl("https://kauth.kakao.com")
+                .defaultHeader(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON_VALUE)
+                .build();
+        return HttpServiceProxyFactory.builderFor(RestClientAdapter.create(rc))
+                .build()
+                .createClient(KakaoAuthClient.class);
+    }
+
+    @Bean
+    public KakaoApiClient kakaoApiClient(RestClient.Builder builder) {
+        RestClient rc = builder
+                .baseUrl("https://kapi.kakao.com")
+                .defaultHeader(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON_VALUE)
+                .build();
+        return HttpServiceProxyFactory.builderFor(RestClientAdapter.create(rc))
+                .build()
+                .createClient(KakaoApiClient.class);
     }
 }
