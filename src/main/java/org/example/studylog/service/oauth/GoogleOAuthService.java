b37edf76711e6dc9db21383585ac3c2f95592e87
정지윤 <samtori3849@gmail.com>
2025-08-22 19:57:16 +0900

    [Feat] googleClient로 unlink 요청을 보내는 GoogleOAuthService 추가

--- src/main/java/org/example/studylog/service/oauth/GoogleOAuthService.java
+++ src/main/java/org/example/studylog/service/oauth/GoogleOAuthService.java
@@ -1,2 +1,16 @@
-package org.example.studylog.service.oauth;public class GoogleOAuthService {
+package org.example.studylog.service.oauth;
+
+import lombok.RequiredArgsConstructor;
+import org.example.studylog.client.GoogleClient;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class GoogleOAuthService {
+
+    private final GoogleClient googleClient;
+
+    public void revoke(String token) {
+        googleClient.unlink(token);
+    }
 }
