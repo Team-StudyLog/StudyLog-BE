ce52fc52a9c4d272082c3be2fafbe20ee9af599b
정지윤 <samtori3849@gmail.com>
2025-08-22 19:55:26 +0900

    [Feat] 구글 OAuth 서버에 토큰 재발급과 토큰 회수(revoke) 요청 보내는 client 추가

--- src/main/java/org/example/studylog/client/GoogleClient.java
+++ src/main/java/org/example/studylog/client/GoogleClient.java
@@ -1,2 +1,22 @@
-package org.example.studylog.client;public interface GoogleClient {
+package org.example.studylog.client;
+
+import org.example.studylog.dto.oauth.OAuthTokenResponse;
+import org.springframework.http.MediaType;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.service.annotation.HttpExchange;
+import org.springframework.web.service.annotation.PostExchange;
+
+@HttpExchange(accept = MediaType.APPLICATION_JSON_VALUE)
+public interface GoogleClient {
+
+    // Refresh 토큰으로 액세스 토큰 재발급
+    @PostExchange(url = "/token", contentType = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+    OAuthTokenResponse reissueToken(@RequestBody MultiValueMap<String, String> form);
+
+    // 토큰 리보크(연결 해제)
+    @PostExchange(url = "/revoke", contentType = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+    void unlink(@RequestParam String token);
+
 }
