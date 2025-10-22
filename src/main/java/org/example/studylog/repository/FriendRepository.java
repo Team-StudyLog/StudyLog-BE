786cc8000be0d6ac9850883f0878515dcefb97b5
y11n <samtori3849@gmail.com>
2025-10-22 16:21:14 +0900

    [Feat] userId와 연관된 friendId 리스트를 반환하는 메서드 추가

--- src/main/java/org/example/studylog/repository/FriendRepository.java
+++ src/main/java/org/example/studylog/repository/FriendRepository.java
@@ -4,7 +4,9 @@ package org.example.studylog.repository;
 import org.example.studylog.entity.Friend;
 import org.example.studylog.entity.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface FriendRepository extends JpaRepository<Friend, Long> {
@@ -14,4 +16,7 @@ public interface FriendRepository extends JpaRepository<Friend, Long> {
     Optional<Friend> findByUserAndFriend(User user, User friend);
 
     long countByUser(User user);
+
+    @Query("SELECT f.friend.id FROM Friend f WHERE f.user.id = :userId")
+    List<Long> findFriendIdsByUserId(Long userId);
 }
